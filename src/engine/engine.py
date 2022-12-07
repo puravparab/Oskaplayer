78adc80164b26dda896189876a1b490db2f7f77a
PuravParab <47409476+PuravParab@users.noreply.github.com>
2022-12-08 00:30:13 +0400

    fixed new board generation

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -1,3 +1,5 @@
+import math
+
 def engine(player_piece, no_of_pieces):
 	if player_piece == "W":
 		ai_piece = "B"
@@ -9,6 +11,7 @@ def engine(player_piece, no_of_pieces):
 
 	Board = make_board(no_of_pieces, player_piece, ai_piece)
 	print(Board)
+	print_board(Board)
 
 """
 Create new board
@@ -22,16 +25,28 @@ def make_board(no_of_pieces, player_piece, ai_piece):
 	length = 2*no_of_pieces - 3
 
 	Board = []
+	row_length = no_of_pieces
 	for i in range(0, length):
 		row = []
-		for j in range(0, no_of_pieces):
+		for j in range(0, row_length):
 			if i == 0:
 				row.append(ai_piece)
 			elif i == length - 1:
 				row.append(player_piece)
 			else:
 				row.append("_")
+				
+		if i < math.floor(length / 2):
+			row_length -= 1
+		else:
+			row_length += 1
 
 		Board.append(row)
 
 	return Board
+
+def print_board(Board):
+	for i in range(0, len(Board)):
+		for j in range(0, len(Board[i])):
+			print(Board[i][j], end=" ")
+		print()
