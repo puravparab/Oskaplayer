431181914aa6bf2cca23daae1411b638bb087089
PuravParab <47409476+PuravParab@users.noreply.github.com>
2022-12-09 06:39:55 +0400

    added getter util to find valid pieces

--- src/engine/player.py
+++ src/engine/player.py
@@ -1,4 +1,5 @@
 import copy
+from .utils import getters
 
 # Handles game logic for human player's turn
 def player_turn(Board, curr_player):
@@ -7,6 +8,8 @@ def player_turn(Board, curr_player):
 	# Ask user to choose a piece and where to move it
 	while True:
 		selected_piece = input("Select piece to move (enter co-ordinates): ")
+
+		print(getters.get_valid_pieces(board_copy, curr_player, True))
 
 		target_location = input("Select a target location (enter co-ordinates): ")
 
--- src/engine/utils/getters.py
+++ src/engine/utils/getters.py
@@ -1,2 +1,19 @@
-def get_valid_pieces(Board, curr_piece):
-	
+# Return a list of coordinates for valid pieces
+def get_valid_pieces(Board, curr_player, human_player):
+	list_of_pieces = [] 
+
+	# Human players can only move pieces that are not on the first row
+	if human_player:
+		for i in range(1, len(Board)):
+			for j in range(0, len(Board[0])):
+				if Board[i][j] == curr_player:
+					list_of_pieces.append([i,j])
+
+	# AI player can only move pieces that are not on the last row
+	elif not human_player:
+		for i in range(0, len(Board) - 1):
+			for j in range(0, len(Board[0])):
+				if Board[i][j] == curr_player:
+					list_of_pieces.append([i,j])
+
+	return list_of_pieces
