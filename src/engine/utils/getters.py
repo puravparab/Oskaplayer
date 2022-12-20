22a22b7a4db0290c33ee66c25be418ef148cc3fc
PuravParab <47409476+PuravParab@users.noreply.github.com>
2022-12-20 06:45:45 +0400

    fixed error in get_valid_pieces where it showed pieces with no valid moves

--- src/engine/utils/getters.py
+++ src/engine/utils/getters.py
@@ -7,14 +7,18 @@ def get_valid_pieces(Board, curr_player, human_player):
 		for i in range(1, len(Board)):
 			for j in range(0, len(Board[0])):
 				if Board[i][j] == curr_player:
-					list_of_pieces.append([i,j])
+					valid_moves = get_valid_moves(Board, curr_player, f'[{i},{j}]', human_player)
+					if valid_moves != []:
+						list_of_pieces.append([i,j])
 
 	# AI player can only move pieces that are not on the last row
 	elif not human_player:
 		for i in range(0, len(Board) - 1):
 			for j in range(0, len(Board[0])):
 				if Board[i][j] == curr_player:
-					list_of_pieces.append([i,j])
+					valid_moves = get_valid_moves(Board, curr_player, f'[{i},{j}]', human_player)
+					if valid_moves != []:
+						list_of_pieces.append([i,j])
 
 	return list_of_pieces
 
